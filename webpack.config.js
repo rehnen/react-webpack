d47d0760161795eb1d29a7422dbc07903a398104
Marcus Rehn <marcus.rehn@hotmail.com>
2018-10-21 18:57:54 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index',
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    output: {
+        path: path.join(__dirname, '/target'),
+        filename: 'index_bundle.js',
+    },
+    devServer: {
+        stats: 'errors-only',
+    },
+    module: {
+        rules: [
+            {
+                test: /(\.js$|\.jsx$)/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env'],
+                    plugins: [require('@babel/plugin-proposal-object-rest-spread')],
+                },
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+    ],
+};
